59287ca357d44e623c2881a84c339291090c9839
Bryan Truong <50316257+yung-upgrade@users.noreply.github.com>
2020-07-13 01:24:41 -0700

    added Tetris class

--- tetris.py
+++ tetris.py
@@ -35,3 +35,29 @@ class Figure:
 
     def rotate(self):
         self.rotation = (self.rotation+1) % len(self.figures[self.type])
+
+class Tetris:
+    level = 2
+    score = 0
+    state = "start"
+    field = []
+    height = 0
+    width = 0
+    x = 100
+    y = 60
+    zoom = 20
+    figure = None
+
+    #Constructor for Tetris class
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+
+        for i in range(height):
+            new_line = []
+            for j in range(width):
+                new_line.append(0)
+            self.field.append(new_line)
+
+    def new_figure(self):
+        self.figure = Figure(3,0)
